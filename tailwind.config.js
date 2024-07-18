f4c4266587eb4931bd1cc36ff188d6bf8505865a
Briian3306 <briian3306@gmail.com>
2024-07-17 23:28:27 -0300

    Update Header layout with fixed position and z-index

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
 
   ],
   theme: {
+    screens: {
+      'sm': '0px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       colors: {
         primary: '#9c8445',
