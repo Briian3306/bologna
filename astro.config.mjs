d191e48b7e383a4e2d94aa739c50d0ecaff1713f
Briian3306 <briian3306@gmail.com>
2024-07-24 15:42:03 -0300

    feat: Add @astrojs/sitemap integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
