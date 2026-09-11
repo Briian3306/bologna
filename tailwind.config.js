25240f75f2d250b5c9a246fda47cd9631750529b
Briian3306 <briian3306@gmail.com>
2026-09-11 14:39:34 -0300

    Refactor Tailwind configuration and update component styles for improved responsiveness and accessibility. Adjust layout and text properties in Card, Title, Header, and Layout components, including enhancements to navigation and button functionality.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,10 @@ export default {
   ],
   theme: {
     screens: {
-      'sm': '0px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'lg': '1280px',
-      // => @media (min-width: 1280px) { ... }
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
     },
     extend: {
       colors: {
